2928ebc69081f5b16014d4f0f5d72de292f63e1f
Firedrake969 <Firedrake969@users.noreply.github.com>
2014-08-07 15:32:49 -0600

    Update pong.js

--- pong/pong.js
+++ pong/pong.js
@@ -1,7 +1,7 @@
 var ballDir = (Math.floor(Math.random() * (360) + 1)) * (Math.PI / 180);
 var player = $('#player');
 var cpu = $("#cpu");
-//var ball = $("#ball");
+var ball = $("#ball");
 var starting = true;
 var dist = 2;
 var playerSpeed = 3;
@@ -25,45 +25,45 @@ function ballBounce(condition) {
 }
 
 function aiMove() {
-    if ($('#ball').position().left > cpu.position().left + 30) {
-        if ($('#ball').position().left - cpu.position().left + 30 < 120) {
-            if ($('#ball').position().top - cpu.position().top < 120) {
+    if ($('#ball').position().left > $('#cpu').position().left + 30) {
+        if ($('#ball').position().left - $('#cpu').position().left + 30 < 120) {
+            if ($('#ball').position().top - $('#cpu').position().top < 120) {
                 cpuSpeed = 2;
             } else {
                 cpuSpeed = 2 - (Math.random() / 2); //from 1.5-2
             }
         } else {
-            if ($('#ball').position().top - cpu.position().top > 250) {
+            if ($('#ball').position().top - $('#cpu').position().top > 250) {
                 cpuSpeed = 1;
-            } else if ($('#ball').position().top - cpu.position().top > 160) {
+            } else if ($('#ball').position().top - $('#cpu').position().top > 160) {
                 cpuSpeed = 1.5;
             } else {
                 cpuSpeed = 2;
             }
         }
     } else {
-        if (cpu.position().left - $('#ball').position().left + 30 < 120) {
-            if ($('#ball').position().top - cpu.position().top < 120) {
+        if ($('#cpu').position().left - $('#ball').position().left + 30 < 120) {
+            if ($('#ball').position().top - $('#cpu').position().top < 120) {
                 cpuSpeed = -2;
             } else {
                 cpuSpeed = -2 + (Math.random() / 2);
             }
         } else {
-            if ($('#ball').position().top - cpu.position().top > 250) {
+            if ($('#ball').position().top - $('#cpu').position().top > 250) {
                 cpuSpeed = -1;
-            } else if ($('#ball').position().top - cpu.position().top > 160) {
+            } else if ($('#ball').position().top - $('#cpu').position().top > 160) {
                 cpuSpeed = -1.5;
             } else {
                 cpuSpeed = -2;
             }
         }
     }
-    cpu.css("left", (cpu.position().left + cpuSpeed) + "px");
-    if (cpu.position().left < 5) {
-        cpu.css("left", "5px");
+    $('#cpu').css("left", ($('#cpu').position().left + cpuSpeed) + "px");
+    if ($('#cpu').position().left < 5) {
+        $('#cpu').css("left", "5px");
     }
-    if (cpu.position().left > 440) {
-        cpu.css("left", "435px");
+    if ($('#cpu').position().left > 440) {
+        $('#cpu').css("left", "435px");
     }
 }
 
@@ -101,20 +101,20 @@ function paddleBounce(condition) {
 */
 
 function paddleBounce() {
-    if ($('#ball').position().left < player.position().left + 70 && $('#ball').position().left + 10 > player.position().left && $('#ball').position().top > 350) {
+    if ($('#ball').position().left < $('#player').position().left + 70 && $('#ball').position().left + 10 > $('#player').position().left && $('#ball').position().top > 350) {
         ballDir = (Math.abs(180 - (ballDir * (180 / Math.PI)))) * (Math.PI / 180);
     }
-    if ($('#ball').position().left < cpu.position().left + 70 && $('#ball').position().left + 10 > cpu.position().left && $('#ball').position().top < 30) {
+    if ($('#ball').position().left < $('#cpu').position().left + 70 && $('#ball').position().left + 10 > $('#cpu').position().left && $('#ball').position().top < 30) {
         ballDir = ((180 + (ballDir * (180 / Math.PI)))) * (Math.PI/180);
     }
 }
 
 /*
 $(document).keydown(function (e) {
-    if (e.keyCode === 68 && player.position().left < 430) {
-        player.css("left", player.position().left + playerSpeed + 'px');
-    } else if (e.keyCode === 65 && player.position().left > 10) {
-        player.css("left", player.position().left - playerSpeed + 'px');
+    if (e.keyCode === 68 && $('#player').position().left < 430) {
+        $('#player').css("left", $('#player').position().left + playerSpeed + 'px');
+    } else if (e.keyCode === 65 && $('#player').position().left > 10) {
+        $('#player').css("left", $('#player').position().left - playerSpeed + 'px');
     }
 });
 */
@@ -144,13 +144,13 @@ $(document).mouseenter(function () {
                 aiMove();
                 detectGoal();
                 //Player smooth movement
-                if (key == 1 && player.position().left < 430) {
-                    player.css("left", player.position().left + playerSpeed + 'px');
-                } else if (key == 2 && player.position().left > 10) {
-                    player.css("left", player.position().left - playerSpeed + 'px');
+                if (key == 1 && $('#player').position().left < 430) {
+                    $('#player').css("left", $('#player').position().left + playerSpeed + 'px');
+                } else if (key == 2 && $('#player').position().left > 10) {
+                    $('#player').css("left", $('#player').position().left - playerSpeed + 'px');
                 }
             }, 5);
         }
         starting = false;
     });
-});
+}); 
