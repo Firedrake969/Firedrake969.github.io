e10ed1081e38b57dc7063d00f22efc5fb5c6e734
Firedrake969 <Firedrake969@users.noreply.github.com>
2014-08-07 11:54:23 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ $(window).resize(function () {
         $('#menu').hide();
     } else if ($(window).width() >= 830) {
         $('#menu').show();
-        $('#menu').css("left", (($(window).width()/2) - 415) + 'px');
+        $('#menu').css("left", (($(window).width() / 2) - 415) + 'px');
     }
+    $('#menu').css('margin-top', (0 - ($('#menu').height() / 2)) + 'px');
 });
 
 $(document).ready(function () {
@@ -12,8 +13,9 @@ $(document).ready(function () {
         $('#menu').hide();
     } else if ($(window).width() >= 830) {
         $('#menu').show();
-        $('#menu').css("left", (($(window).width()/2) - 415) + 'px');
+        $('#menu').css("left", (($(window).width() / 2) - 415) + 'px');
     }
+    $('#menu').css('margin-top', (0 - ($('#menu').height() / 2)) + 'px');
 });
 
 //825:  Minimum width before hiding menu
