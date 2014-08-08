008609507e9e5565a6613ba5b032c67935bf693b
Firedrake969 <Firedrake969@users.noreply.github.com>
2014-08-07 18:26:40 -0600

    Update pong.js

--- pong/pong.js
+++ pong/pong.js
@@ -151,3 +151,23 @@ $(document).mouseenter(function () {
         starting = false;
     });
 }); 
+
+$(window).resize(function () {
+    if ($(window).width() < 830) {
+        $('#menu').hide();
+    } else if ($(window).width() >= 830) {
+        $('#menu').show();
+        $('#menu').css("left", (($(window).width() / 2) - 415) + 'px');
+    }
+    $('#menu').css('margin-top', (0 - ($('#menu').height() / 2)) + 'px');
+});
+
+$(document).ready(function () {
+    if ($(window).width() < 830) {
+        $('#menu').hide();
+    } else if ($(window).width() >= 830) {
+        $('#menu').show();
+        $('#menu').css("left", (($(window).width() / 2) - 415) + 'px');
+    }
+    $('#menu').css('margin-top', (0 - ($('#menu').height() / 2)) + 'px');
+});
