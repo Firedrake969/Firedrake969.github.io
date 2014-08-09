29bab403a1c1d2f632a7b53110489806f529b38b
Firedrake969 <Firedrake969@users.noreply.github.com>
2014-08-09 14:46:35 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,20 +4,21 @@ order:
 body, .mlink:link, .mlink:visited, .link:link, .link:visited, .textbox, #top, #menu
 body, .textbox, #top, and #menu are background-color; the rest are color
 */
-    function theme(c1, c2, c3, c4, c5, c6, c7, c8) {
-        $('body').css("background-color", c1);
-        $('.mlink:link').css("color", c2);
-        $('.mlink:visited').css("color", c3);
-        $('.link:link').css("color", c4);
-        $('.link:visited').css("color", c5);
-        $('.textbox').css("background-color", c6);
-        $('#top').css("background-color", c7);
-        $('#menu').css("background-color", c8);
-        window.scrollTo(0, 0);
-    }
+function theme(c1, c2, c3, c4, c5, c6, c7, c8) {
+    $('body').css("background-color", c1);
+    $('.mlink:link').css("color", c2);
+    $('.mlink:visited').css("color", c3);
+    $('.link:link').css("color", c4);
+    $('.link:visited').css("color", c5);
+    $('.textbox').css("background-color", c6);
+    $('#top').css("background-color", c7);
+    $('#menu').css("background-color", c8);
+    window.scrollTo(0, 0);
+}
 $('#blue').click(function () {
     theme(blue[0], blue[1], blue[2], blue[3], blue[4], blue[5], blue[6], blue[7]);
     document.cookie = 'color=blue; expires=' + CookieDate.toGMTString( ) + ';';
+    alert(document.cookie);
 });
 
 $(window).resize(function () {
