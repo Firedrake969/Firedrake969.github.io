e6872cf680bca5a971d90d7f78f3322540caf8d1
Firedrake969 <Firedrake969@users.noreply.github.com>
2014-08-10 16:00:47 -0600

    Update main.js

--- main.js
+++ main.js
@@ -37,13 +37,9 @@ $(document).ready(function () {
     $('#menu').css('margin-top', (0 - ($('#menu').height() / 2)) + 'px');
     $('#blue').click(function () {
         theme(blue[0], blue[1], blue[2], blue[3], blue[4], blue[5], blue[6], blue[7]);
-        eraseCookie("color");
-        createCookie("color", "blue", 3650);
     });
     $('#gray').click(function () {
         theme(gray[0], gray[1], gray[2], gray[3], gray[4], gray[5], gray[6], gray[7]);
-        eraseCookie("color");
-        createCookie("color", "blue", 3650);
     });
 });
 
